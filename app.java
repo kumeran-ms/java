472a4d9cf54f854ffdfc2bad0898aaec3d066014
kumeran-ms <srdhrn.kumeran@gmail.com>
2023-09-05 16:37:33 +0530

    changed the value to 200

--- app.java
+++ app.java
@@ -1,6 +1,6 @@
 class App{
     public static void main(String[] args) {
-        for (int i = 0; i < 500; i++) {
+        for (int i = 0; i < 200; i++) {
             System.out.println(i);
         }
     }
