f2682dc86203afc1e251f8458282afcfef06088f
kumeran-ms <srdhrn.kumeran@gmail.com>
2023-09-05 15:52:53 +0530

    java

---
+++ app.java
@@ -0,0 +1,10 @@
+class App{
+    public static void main(String[] args) {
+        for (int i = 0; i < 500; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+// javac app.java
+// java App
